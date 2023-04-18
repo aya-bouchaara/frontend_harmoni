f2317813481621f389c898028e5846b9b1d798af
dell <yankeeayaayouya28mars@gmail.com>
2023-04-18 15:50:19 +0200

    les interfaces

---
+++ app/src/main/java/com/example/harmoni/MainActivity2.java
@@ -0,0 +1,39 @@
+package com.example.harmoni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button btn2,btn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btn2=findViewById(R.id.button2);
+        btn3=findViewById(R.id.button3);
+
+
+        //listener for button 'Log in'
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it=new Intent(MainActivity2.this, login.class);
+                startActivity(it);
+
+            }
+        });
+        //listener for button 'Sign up'
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity2.this, register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
