8136a6dfda4b259d9589a945b095571a6bbebf38
Barkallil Rayan <93799467+RayanBarkallil@users.noreply.github.com>
2023-04-20 04:20:36 +0000

    account image

---
+++ app/src/main/java/com/example/harmoni/account_photo.java
@@ -0,0 +1,132 @@
+package com.example.harmoni;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+
+public class account_photo extends AppCompatActivity {
+
+    public ImageView pick;
+    public static final int CAMERA_REQUEST = 100;
+    public static final int STORAGE_REQUEST = 101;
+    String cameraPermission[];
+    String storagePermission[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_photo);
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        pick = (ImageView) findViewById(R.id.imageView7);
+        pick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int picd = 0;
+                if (picd == 0) {
+                    if (!checkCameraPermission()) {
+                        requestCameraPermission();
+                    } else {
+                        pickFromGallery();
+                    }
+                }else if (picd == 1) {
+                        if (!checkStoragePermission()) {
+                            requestStoragePersmission();
+                        } else {
+                            pickFromGallery();
+                        }
+                    }
+                }
+
+        });
+    }
+
+    private void requestStoragePersmission() {
+        requestPermissions(storagePermission,STORAGE_REQUEST);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result= (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED));
+        return result;
+    }
+
+    private void pickFromGallery() { CropImage.activity().start(this);}
+
+    private void requestCameraPermission() {requestPermissions(cameraPermission,CAMERA_REQUEST);}
+
+    private boolean checkCameraPermission(){
+        boolean result = ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)==(PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result=CropImage.getActivityResult(data);
+            if(resultCode==RESULT_OK){
+                Uri resultUri=result.getUri();
+                Picasso.get().load(resultUri).into(pick);
+
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case CAMERA_REQUEST:{
+                if (grantResults.length>0){
+                    boolean camera_accepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageaccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (camera_accepted && writeStorageaccepted) {
+                        pickFromGallery();
+                    } else {
+                        Toast.makeText(this, "Please Enable Camera and Storage Permissions", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+            break;
+            case STORAGE_REQUEST: {
+                if (grantResults.length > 0) {
+                    boolean writeStorageaccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (writeStorageaccepted) {
+                        pickFromGallery();
+                    } else {
+                        Toast.makeText(this, "Please Enable Storage Permissions", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/harmoni/login.java
+++ app/src/main/java/com/example/harmoni/login.java
@@ -1,14 +1,27 @@
 package com.example.harmoni;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class login extends AppCompatActivity {
+
+    TextView Forgotpass; //Forgotpassword
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Forgotpass =findViewById(R.id.textView5);
+        Forgotpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(login.this , forgotpass.class);
+                startActivity(intent);
+            }
+        });
     }
 }
