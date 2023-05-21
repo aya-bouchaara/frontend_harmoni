eeab898130a53217cbefc896a41c1cc72c6e4a62
dell <yankeeayaayouya28mars@gmail.com>
2023-05-21 17:49:40 +0100

    commit dashboard

--- app/src/main/java/com/example/harmoni/dashboard2.java
+++ app/src/main/java/com/example/harmoni/dashboard2.java
@@ -1,38 +1,58 @@
 package com.example.harmoni;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+
+import java.lang.annotation.Native;
+
 
 public class dashboard2 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private DrawerLayout drawerLayout;
+
+
+
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard2);
 
-        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
-        setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
-                R.string.close_nav);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new homeFragment()).commit();
@@ -40,38 +60,36 @@ public class dashboard2 extends AppCompatActivity implements NavigationView.OnNa
         }
     }
 
+
+
+
+
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new homeFragment()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new homeFragment()).commit();
                 break;
-
-            case R.id.nav_settings:
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new SettingsFragment()).commit();
-                break;
-
             case R.id.nav_playlists:
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new playlistsFragment()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new playlistsFragment()).commit();
                 break;
-
             case R.id.nav_chats:
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new chatsFragment()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new chatsFragment()).commit();
                 break;
             case R.id.nav_suggestions:
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new suggestionsFragment()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new suggestionsFragment()).commit();
                 break;
-
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new SettingsFragment()).commit();
+                break;
             case R.id.nav_logout:
-                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this , "logout!", Toast.LENGTH_SHORT).show();
                 break;
         }
-
-        drawerLayout.closeDrawer(GravityCompat.START);
+       drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
-
-    @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
--- app/src/main/java/com/example/harmoni/homeFragment.java
+++ app/src/main/java/com/example/harmoni/homeFragment.java
@@ -1,23 +1,105 @@
 package com.example.harmoni;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+import com.spotify.protocol.client.Subscription;
 
 
 public class homeFragment extends Fragment {
-
-
-
+    private static final String CLIENT_ID = "b8f0cac7dd104537901f8d0fed64da41";
+    private static final String REDIRECT_URI = "http://localhost:9000/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+    private Button mPlayButton;
+    private boolean mIsPlaying = false;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        mPlayButton = rootView.findViewById(R.id.play_button);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mIsPlaying) {
+                    pausePlayback();
+                } else {
+                    startPlayback();
+                }
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Set the connection parameters
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(requireContext(), connectionParams,
+                new Connector.ConnectionListener() {
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("homeFragment", "Connected! Yay!");
+
+                        // Subscribe to PlayerState
+                        mSpotifyAppRemote.getPlayerApi().subscribeToPlayerState().setEventCallback(new Subscription.EventCallback<PlayerState>() {
+                            @Override
+                            public void onEvent(PlayerState playerState) {
+                                Track track = playerState.track;
+                                Log.d("homeFragment", track.name + " by " + track.artist.name);
+                                mIsPlaying = playerState.isPaused == false;
+                                updateButton();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("homeFragment", throwable.getMessage(), throwable);
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    private void startPlayback() {
+        if (mSpotifyAppRemote != null) {
+            mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+        }
+    }
+
+    private void pausePlayback() {
+        if (mSpotifyAppRemote != null) {
+            mSpotifyAppRemote.getPlayerApi().pause();
+        }
+    }
+
+    private void updateButton() {
+        if (mIsPlaying) {
+            mPlayButton.setText("Pause Playlist");
+        } else {
+            mPlayButton.setText("Play Playlist");
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
